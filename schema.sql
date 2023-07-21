3619a350c4eaa462dbd45ed4500da3002b8feaa3
toyman640 <falakoadebayor@gmail.com>
2023-07-21 15:51:26 +0100

    Add tasks to respective files

--- schema.sql
+++ schema.sql
@@ -16,3 +16,24 @@ ALTER table animals ADD COLUMN id SERIAL PRIMARY KEY;
 ALTER table animals DROP COLUMN species;
 ALTER table animals ADD COLUMN species_Id int references species(id);
 ALTER table animals ADD COLUMN owner_Id int references owners(id);
+
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+  visit_id SERIAL PRIMARY KEY,
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE
+);
